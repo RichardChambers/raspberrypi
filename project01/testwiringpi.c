f74b54e6baa1d4c9ad8f1f364732b2da1ba44913
Richard Chambers <rickchambers.geo@gmail.com>
2017-11-18 11:15:52 -0500

    initial upload of project01 source to blink an LED
    This C source file for Raspberry Pi 3 will blink an LED that is connected to GPIO #17.
    
    See Lesson 3 for Osoyoo Raspberry Pi Start Kit at URL: http://osoyoo.com/2017/06/22/intall-wiringpi/

---
+++ project01/testwiringpi.c
@@ -0,0 +1,52 @@
+/*
+ *   Test program for Raspberry Pi from URL
+ *     http://osoyoo.com/2017/06/22/intall-wiringpi/
+ *
+ *   This test program performs a test of a simple LED wiring
+ *   that is connected to GPIO #17 to turn an LED on and off.
+ *
+ *   Compile with the following command line:
+ *     cc testwiringpi.c -lwiringPi
+ *
+ *   This command line compiles this test program and links it with the
+ *   wiringPi.lib library for manipulating the GPIO pins of a Raspberry Pi.
+*/
+
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define LEDPIN  0  // LEDPIN is wiring Pi Pin #0 or physical pin #11 or GPIO #17
+
+void turn_on (int gpio) {
+	digitalWrite (gpio, LOW);
+}
+
+void turn_off (int gpio) {
+	digitalWrite (gpio, HIGH);
+}
+
+int main ()
+{
+	int iCount = 10;
+
+	if (wiringPiSetup() == -1) {
+		printf ("Setup wiringPi Failed!\n");
+		return -1;
+	}
+
+	pinMode (WPI_MODE_PINS, OUTPUT);
+	
+	while (iCount > 0) {
+		digitalWrite (LEDPIN, LOW);  // turn the LED on
+		printf ("LED On ... "); fflush (stdout);
+		delay (1000);
+		digitalWrite (LEDPIN, HIGH); // turn the LED off
+		printf ("LED Off\n");
+		delay (500);
+		iCount--;
+	}
+
+	digitalWrite (LEDPIN, HIGH);  // make sure the LED is off
+	delay (500);
+	return 0;
+}
