b5acf23b174655b7adea198650257fbe41e5b358
Richard Chambers <rickchambers.geo@gmail.com>
2017-11-19 11:00:10 -0500

    starting lesson #5 add the make files for both project01 and project02. add the C source code file for project02. update the README.md file with the gpio readall output to show the various GPIO pin labels using by wiringPi library.
    The file flowwiring.c in project02 is incomplete at this stage but does
    compile.

---
+++ project02/flowwiring.c
@@ -0,0 +1,51 @@
+//
+//  flowwiring.c - Lesson #5 for Osoyoo Raspberry Pi Starter Kit.
+//    http://osoyoo.com/2017/06/25/raspberry-pi-flowing-led/
+
+#include <stdio.h>
+
+#include <wiringPi.h>
+
+// Following two functions are used to turn an LED on or off.
+// These functions assume that the LED is connected to a power
+// source, the 3.3v pin, and the other end is connected to a
+// GPIO pin on the Raspberry Pi GPIO board.
+// To light the LED current must flow by driving the specified
+// GPIO pin low. To unlight the LED current must stop flowing
+// by driving the specified GPIO pin high.
+
+void turn_on (int pin)
+{
+	digitalWrite (pin, LOW);
+}
+
+void turn_off (int pin)
+{
+	digitalWrite (pin, HIGH);
+}
+
+
+// A pin list is an array of pin numbers for the LEDs that are being
+// used in this lesson. The pin numbers are positive values from 
+// zero to the number of pins. A negative value, usually -1, is used
+// to indicate the end of the list.
+void setup_pins (int *pin_list)
+{
+	for (; *pin_list >= 0; pin_list++) {
+		pinMode (*pin_list, OUTPUT);
+		turn_off (*pin_list);
+	}
+}
+
+int main ()
+{
+	int pin_list[] = {0, 1};
+
+	if (wiringPiSetup() == -1) {
+		printf ("Setup wiringPi failed! Check Setup.\n");
+		return -1;
+	}
+
+
+	return 0;
+}
